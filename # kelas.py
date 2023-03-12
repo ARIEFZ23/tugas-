f46ae9d87856757f66cce3e23699e004012c5114
ARIEFZ23 <127678675+ARIEFZ23@users.noreply.github.com>
2023-03-13 07:15:57 +0800

    Add files via upload

---
+++ # kelas.py
@@ -0,0 +1,22 @@
+# kelas
+class Marvel:
+    pass
+
+# object
+Marvel1 = Marvel()
+Marvel2 = Marvel()
+Marvel3 = Marvel()
+
+Marvel1. name = "Rozy Madilao"
+Marvel1. healt = "1000"
+
+Marvel2.name = "Madilao Rozy"
+Marvel2.name = "800"
+
+Marvel3.name = "Izam Daeng"
+Marvel3.name = "900"
+
+#pemanggilan
+print(marvel1)
+print(maarvel1._dict_)
+print(marvel1.name)
